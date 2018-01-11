e2ecabe4c5d6135dcfff69147655d923f4a19477
Rutger Lubbers <rutger.lubbers@qnh.nl>
2018-01-11 15:21:20 +0100

    update installers.

--- installers/etc-hosts/provision.sh
+++ installers/etc-hosts/provision.sh
@@ -6,8 +6,8 @@ cat > /etc/hosts << EOF
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6 hawaii hawaii2
 
 # Hawaii hostnames
-127.0.0.1		kahuna-src.qnh.nl kahuna-target.qnh.nl doc-src.qnh.nl doc-target.qnh.nl kahu-src.qnh.nl kahu-target.qnh.nl
-127.0.0.1		guide-src.qnh.nl guide-target.qnh.nl minis-src.qnh.nl minis-target.qnh.nl minis-uc.qnh.nl inbucket.qnh.nl
+10.0.0.2		kahuna-src.qnh.nl kahuna-target.qnh.nl doc-src.qnh.nl doc-target.qnh.nl kahu-src.qnh.nl kahu-target.qnh.nl
+10.0.0.2		guide-src.qnh.nl guide-target.qnh.nl minis-src.qnh.nl minis-target.qnh.nl minis-uc.qnh.nl inbucket.qnh.nl
 
 127.0.0.1   hawaii-cucumber-2
 EOF
